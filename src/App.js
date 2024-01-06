50534f1daccc584104ea2ee1df819a6f8dbaccb3
jesmincms <fimran2011@gmail.com>
2024-01-06 15:53:56 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import HomePage from "./pages/home";
+import PricingPage from "./pages/pricing";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+<BrowserRouter>
+<Routes>
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/pricing" element={<PricingPage/>} />
+  </Route>
+</Routes>
+</BrowserRouter>
+    
   );
 }
---
+++ src/pages/pricing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+export default function PricingPage() {
+  return (
+<>
+<Header />
+<Footer />
+</>
+  );
+}
